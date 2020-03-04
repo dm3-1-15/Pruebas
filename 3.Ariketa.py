531954e9761107dd3c15ee1385fc451fff10bf73
dm3-1-15 <61787713+dm3-1-15@users.noreply.github.com>
2020-03-04 14:22:15 +0100

    Revert "Delete 3.Ariketa.py"
    This reverts commit fd6faf3153124789d5ea3c31768d8769e7b83742.

---
+++ 3.Ariketa.py
@@ -0,0 +1,12 @@
+def batazbestekoa (zenbakiak):
+    totala = 0
+    count = 0
+    for x in zenbakiak:
+        totala += x
+        count += 1
+    totala = totala/count
+    print(totala)
+zenbakiak = [1,2,3,4,5,6]
+zenbakiak.remove(4)
+print(zenbakiak)
+hola buenos dias
